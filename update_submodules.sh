438326387e0079dd53a78db98057322daa282e67
NaKyle Wright <nakyle.wright@gmail.com>
2015-01-23 14:19:58 -0600

    Adds script to quickly update submodules.

---
+++ update_submodules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+modules=("oh-my-zsh"
+         "tmux/plugins/tpm"
+         "vim/bundle/editorconfig-vim"
+         "vim/bundle/rainbow-parentheses"
+         "vim/bundle/vim-airline"
+         "vim/bundle/vim-clojure-statuc"
+         "vim/bundle/vim-colors-solarized"
+         "vim/bundle/vim-javascript-syntax"
+         "vim/bundle/vim-json"
+         "vim/bundle/vim-markdown"
+         "vim/bundle/vim-pathogen")
+
+for module in "${modules[@]}"; do
+    pushd $module
+    git pull origin master
+    popd > /dev/null
+done
+
+pushd "vim/bundle/python-mode"
+git pull origin develop
+popd > /dev/null
