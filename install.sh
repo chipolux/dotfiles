70fbd85118399f5ca0f3a926e895b3f39d472e75
NaKyle Wright <nakyle.wright@gmail.com>
2015-03-22 14:43:13 -0500

    Reworks install to provide minimal option, adds minimal configs.

--- install.sh
+++ install.sh
@@ -1,47 +1,102 @@
 #!/bin/bash
 
-files=("bashrc"
-       "profile"
-       "bash_profile"
-       "gitconfig"
-       "gitignore"
-       "tmux.conf"
-       "vimrc"
-       "zshrc"
-       "mailcap")
+function setup-minimal () {
+    echo "Moving minimal dotfiles!"
+    mv vimrc-min vimrc
+    mv bashrc-min bashrc
+    mv tmux.conf-min tmux.conf
 
-for file in "${files[@]}"; do
-    if [ -e $HOME/.$file -a ! -h $HOME/.$file ]; then
-        mv -f $HOME/.$file $HOME/.$file.old
-    fi
-    ln -snf "$(pwd)/$file" $HOME/.$file
-done
+    echo "Moving dotfiles!"
+    move-dot-files \
+        "bashrc" \
+        "profile" \
+        "bash_profile" \
+        "gitconfig" \
+        "gitignore" \
+        "tmux.conf" \
+        "vimrc" \
+        "zshrc"
 
-folders=("tmux"
-         "vim"
-         "oh-my-zsh"
-         "mutt")
+    echo "Moving dotfolders!"
+    move-dot-folders \
+        "tmux" \
+        "vim" \
+        "oh-my-zsh"
 
-for folder in "${folders[@]}"; do
-    if [ -d $HOME/.$folder -a ! -h $HOME/.$folder ]; then
-        mv -f $HOME/.$folder $HOME/.$folder.old
-    fi
-    ln -snfF "$(pwd)/$folder" $HOME/.$folder
-done
+    echo "Getting submodules!"
+    git submodule update --init --recursive
 
-weechat=("irc.conf"
-         "weechat.conf")
+    echo "Installing vim plugins!"
+    vim +PluginInstall +qall
+}
 
-for file in "${weechat[@]}"; do
-    if [ -e $HOME/.weechat/$file ]; then
-        mv -f $HOME/.weechat/$file $HOME/.weechat/$file.old
-        python $(pwd)/weechat/set_options.py \
-            $HOME/.weechat/$file.old \
-            $(pwd)/weechat/$file \
-            $HOME/.weechat/$file
-    fi
-done
+function setup-normal () {
+    echo "Moving dotfiles!"
+    move-dot-files \
+        "bashrc" \
+        "profile" \
+        "bash_profile" \
+        "gitconfig" \
+        "gitignore" \
+        "tmux.conf" \
+        "vimrc" \
+        "zshrc" \
+        "mailcap"
 
-git submodule update --init --recursive
+    echo "Moving dotfolders!"
+    move-dot-folders \
+        "tmux" \
+        "vim" \
+        "oh-my-zsh" \
+        "mutt"
 
-#vim +PluginInstall +qall
+    echo "Moving weechat stuff!"
+    move-weechat-stuff
+
+    echo "Getting submodules!"
+    git submodule update --init --recursive
+
+    echo "Installing vim plugins!"
+    vim +PluginInstall +qall
+}
+
+function move-dot-files () {
+    for file in $@; do
+        if [ -e $HOME/.$file -a ! -h $HOME/.$file ]; then
+            mv -f $HOME/.$file $HOME/.$file.old
+        fi
+        ln -snf "$(pwd)/$file" $HOME/.$file
+    done
+}
+
+function move-dot-folders () {
+    for folder in $@; do
+        if [ -d $HOME/.$folder -a ! -h $HOME/.$folder ]; then
+            mv -f $HOME/.$folder $HOME/.$folder.old
+        fi
+        ln -snfF "$(pwd)/$folder" $HOME/.$folder
+    done
+}
+
+function move-weechat-stuff () {
+    weechat=("irc.conf"
+             "weechat.conf")
+
+    for file in "${weechat[@]}"; do
+        if [ -e $HOME/.weechat/$file ]; then
+            mv -f $HOME/.weechat/$file $HOME/.weechat/$file.old
+            python $(pwd)/weechat/set_options.py \
+                $HOME/.weechat/$file.old \
+                $(pwd)/weechat/$file \
+                $HOME/.weechat/$file
+        fi
+    done
+}
+
+if [ "$1" == "minimal" ]; then
+    echo "Minimal setup..."
+    setup-minimal
+else
+    echo "Standard setup..."
+    setup-normal
+fi
