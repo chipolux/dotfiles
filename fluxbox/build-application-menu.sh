03f1553da543b28944a0699955b35b9ea06bba3e
NaKyle Wright <nakyle.wright@gmail.com>
2016-11-03 19:06:34 -0500

    Add chrome to app list.

--- fluxbox/build-application-menu.sh
+++ fluxbox/build-application-menu.sh
@@ -15,6 +15,11 @@ if [ $app_path ]; then
     echo "[exec] (Firefox) {$app_path} </usr/share/pixmaps/firefox.png>" >> $menu
 fi
 
+app_path=$(which google-chrome)
+if [ $app_path ]; then
+    echo "[exec] (Chrome) {$app_path} </usr/share/icons/hicolor/32x32/apps/google-chrome.png>" >> $menu
+fi
+
 app_path=$(which spyder)
 if [ $app_path ]; then
     echo "[exec] (Spyder) {$app_path} </usr/share/pixmaps/spyder.png>" >> $menu
