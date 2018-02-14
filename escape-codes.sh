b34608fd044d98d24310e84638c085a6a8ea13f3
NaKyle Wright <nakyle.wright@gmail.com>
2018-02-13 18:08:05 -0600

    Add a file to help check if certain escape codes work.

---
+++ escape-codes.sh
@@ -0,0 +1,8 @@
+# source this file to test different escape codes
+# for more formatting stuff see https://misc.flogisoft.com/bash/tip_colors_and_formatting
+echo -e "\e[1mbold\e[0m"
+echo -e "\e[3mitalic\e[0m"
+echo -e "\e[4munderline\e[0m"
+echo -e "\e[9mstrikethrough\e[0m"
+echo -e "\e[34mblue\e[0m"
+echo -e "\x1B[31mred\e[0m"
