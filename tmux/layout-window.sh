3c3771d87b39cb7e3f5766faaef1a6766d77d62a
NaKyle Wright <nakyle.wright@gmail.com>
2018-04-25 15:56:39 -0500

    Min width of right hand panes should be 80 columns.

--- tmux/layout-window.sh
+++ tmux/layout-window.sh
@@ -1,6 +1,6 @@
 # Calculate right panel width as 1/3rd of window width min 100
 WIDTH=$(expr $(tmux display -p '#{window_width}') / 3)
-WIDTH=$((WIDTH > 100 ? WIDTH : 100))
+WIDTH=$((WIDTH > 80 ? WIDTH : 80))
 
 # Layout and resize panes
 tmux select-layout main-vertical
