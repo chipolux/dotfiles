6608bc73667f40c8f7e766a1d08ae381cafb7ae4
NaKyle Wright <nakyle.wright@gmail.com>
2018-11-03 16:09:25 -0500

    Add git aliases i use on Linux/macOS.

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,16 @@
 Set-PSReadlineOption -BellStyle None
 if (Get-Module -Name posh-git -ListAvailable) {Import-Module posh-git}
+
+Remove-Item Alias:gc -Force
+Remove-Item Alias:gl -Force
+Remove-Item Alias:gp -Force
+
+function g {cmd /c $((@("git") + $args) -Join " ")}
+function ga {cmd /c $((@("git", "add") + $args) -Join " ")}
+function gb {cmd /c $((@("git", "branch") + $args) -Join " ")}
+function gc {cmd /c $((@("git", "commit") + $args) -Join " ")}
+function gco {cmd /c $((@("git", "checkout") + $args) -Join " ")}
+function gd {cmd /c $((@("git", "diff") + $args) -Join " ")}
+function gl {cmd /c $((@("git", "pull") + $args) -Join " ")}
+function gp {cmd /c $((@("git", "push") + $args) -Join " ")}
+function gst {cmd /c $((@("git", "status") + $args) -Join " ")}
